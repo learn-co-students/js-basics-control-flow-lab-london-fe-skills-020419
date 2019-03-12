09f155b81861900bba630b06a645cd1123230b61
Jm <jonathan.p.middleton@gmail.com>
2019-03-12 11:48:02 +0000

    Done.

--- index.js
+++ index.js
@@ -2,19 +2,16 @@
 function scuberGreetingForFeet(feet) {
   
 if (feet<=400) {
-  return "This one is on me!"
-} else if (feet>2500) {
-  return "No can do."
-} else {
-  return "I will gladly take your thirty bucks."
-}
-  
-  return 
+  return "This one is on me!";
+  } else if (feet>2500) {
+  return "No can do.";
+  } else {
+  return "I will gladly take your thirty bucks.";
+  }
 }
 
 function ternaryCheckCity(city) {
-  return city=='NYC' ?  "Ok, sounds good." : "No go."  
-
+  return city=='NYC' ?  "Ok, sounds good." : "No go.";  
 }
 
 function switchOnCharmFromTip(tip) {
